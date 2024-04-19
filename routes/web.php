0c52454924f0a870736631a2ae24a5f4f86a0231
PRINCE <princenagac12@gmail.com>
2024-04-20 01:12:59 +0800

    updated Readme

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,65 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+class Task_Data
+
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at,
+    ){
+    }
+}
+
+$task = [
+    
+    new Task_Data(
+        1,
+        'Buy groceries',
+        'Task 1 description',
+        'Task 1 long description',
+        false,
+        '2023-03-01 12:00:00',
+        '2023-03-01 12:00:00'
+    ),
+
+    new Task_Data(
+        2,
+    'Sell old stuff',
+    'Task 2 description',
+    null,
+    false,
+    '2023-03-02 12:00:00',
+    '2023-03-02 12:00:00'
+    ),
+
+    new Task_Data(
+        3,
+        'Learn programming',
+        'Task 3 description',
+        'Task 3 long description',
+        true,
+        '2023-03-03 12:00:00',
+        '2023-03-03 12:00:00'
+    ),
+
+    new Task_Data(
+        4,
+        'Take dogs for a walk',
+        'Task 4 description',
+        null,
+        false,
+        '2023-03-04 12:00:00',
+        '2023-03-04 12:00:00'
+    ),
+];
+
+
 
 Route::get('/', function () {
     return view('welcome');

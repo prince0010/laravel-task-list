fafa697f43ddacf6316162f3ef5e9e27a0e1ae3e
PRINCE <princenagac12@gmail.com>
2024-04-22 07:54:28 +0800

    Added Layout Blade Template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel 10 Task List App</title>
+</head>
+<body>
+    <h1>
+        @yield('title')
+    </h1>
+
+    <div>
+        <p>
+        @yield('content')
+        </p>
+    </div>
+</body>
+</html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,9 +1,12 @@
 <!-- The $tasks is the name of the second parameter in the web.php in the Route::get('/tasks/{id}', function($id) use($tasks) ENDPOINT. IT IS HERE return view('show', ['tasks' => $task ]); 
 The 'tasks' there is the one who youll call inthe blade template or template iheritance for the common layout since the 'tasks' is set as a container in the $task function in the object array since it became object array because of the
 collect() function -->
+@extends('layouts.app')
+
+@section('title', $tasks-> title  )
 
 
-<h1> {{ $tasks-> title }}</h1>
+@section('content')
 <p> {{ $tasks-> description }}</p>
 
 @if ($tasks->long_description)
@@ -12,3 +15,5 @@ collect() function -->
 
 <p> {{ $tasks-> created_at }}</p>
 <p> {{ $tasks-> updated_at }}</p>
+
+@endsection
